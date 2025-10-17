a37a473d781822257519ee1bfd982cd285c60c9f
karishmagdly27 <karishmagdly27@gmail.com>
2025-10-16 23:04:31 -0400

    push

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,4 @@
+# VPC
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -5,11 +6,13 @@ resource "aws_vpc" "main" {
   tags = { Name = "ecs_vpc" }
 }
 
+# Internet Gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags = { Name = "ecs_igw" }
+  tags   = { Name = "ecs_igw" }
 }
 
+# Public Subnet 1
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[0]
@@ -18,13 +21,24 @@ resource "aws_subnet" "public_1" {
   tags = { Name = "public_subnet_1" }
 }
 
+# Public Subnet 2
+resource "aws_subnet" "public_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[1]
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zones[1]
+  tags = { Name = "public_subnet_2" }
+}
+
+# Private Subnet 1
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidrs[0]
-  availability_zone = var.availability_zones[1]
+  availability_zone = var.availability_zones[0]
   tags = { Name = "private_subnet_1" }
 }
 
+# Public Route Table
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -36,7 +50,13 @@ resource "aws_route_table" "public_rt" {
   tags = { Name = "public_rt" }
 }
 
-resource "aws_route_table_association" "public_assoc" {
+# Associate both public subnets
+resource "aws_route_table_association" "public_assoc_1" {
   subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public_rt.id
 }
