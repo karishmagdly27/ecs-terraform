78406e97714eda728f29c08f5df4d8289fbccbf9
karishmagdly27 <karishmagdly27@gmail.com>
2025-10-16 23:00:53 -0400

    load

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "app_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public_1.id]
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id] # two subnets in different AZs
 }
 
 resource "aws_lb_target_group" "app_tg" {
