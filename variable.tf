10cd5913595d686595792d00c1388856ad3846ff
karishmagdly27 <karishmagdly27@gmail.com>
2025-10-18 13:13:12 -0400

    push

--- variable.tf
+++ variable.tf
@@ -12,28 +12,24 @@ variable "aws_account_id" {
   default     = "597406702314"
 }
 
-# VPC and subnet CIDR blocks
 variable "vpc_cidr" {
-  description = "VPC CIDR block"
-  type        = string
-  default     = "10.0.0.0/16"
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "List of public subnet CIDRs"
   type        = list(string)
-  default     = ["10.0.1.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "List of private subnet CIDRs"
   type        = list(string)
-  default     = ["10.0.2.0/24"]
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
-# Availability zones to use (us-west-2)
 variable "availability_zones" {
-  description = "Availability zones to place subnets in. Use us-west-2.*"
+  description = "Availability zones to use"
   type        = list(string)
   default     = ["us-west-2a", "us-west-2b"]
 }
